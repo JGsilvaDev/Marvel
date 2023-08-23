7a0d91ca257d68411ded1886d86f112f22d7d866
João Gabriel <jgoliveira445@gmail.com>
2023-08-23 15:42:17 -0300

    feat: todos os dados

--- api.py
+++ api.py
@@ -1,15 +1,38 @@
 import pandas as pd
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
+dados = pd.read_csv('dados.csv',sep=';')
+dados = dados.dropna()
+dicionario = dados.to_dict(orient='records')
 
 @app.route('/')
-def homepage():
-
-    dados = pd.read_csv('dados.csv',sep=';')
-
-    dicionario = dados.to_dict(orient='records')
-
+def obter_dados_como_json():
     return jsonify(dicionario)
 
-app.run(host='0.0.0.0')
+@app.route('/info')
+def info():
+    name = request.args.get('name')
+    if name is None:
+        return "O parâmetro 'nome' é obrigatório.", 400
+
+    info = [registro for registro in dicionario if registro['Name'] == name]
+
+    return jsonify(info)
+
+@app.route('/info/classes')
+def classes():
+    classes = request.args.get('class')
+    if classes is None:
+        return "O parâmetro 'class' é obrigatório.", 400
+    
+    # info = [registro for registro in dicionario if registro['Class'] == classes]
+
+    info = pd.read_json('dados.json')
+
+    return jsonify(info)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
